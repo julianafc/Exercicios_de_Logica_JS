24ef24bd3c2e0771dd2b7ecc1017aaadda677879
Juliana <julianafigueirac@gmail.com>
2025-08-21 23:31:20 -0300

    feat: Operadores - Hora de Codar

---
+++ Hora de codar/Operadores.js
@@ -0,0 +1,14 @@
+const prompt = require('prompt-sync')();
+
+let number1 = prompt("Digite o primeiro número:");
+let number2 = prompt("Digite o segundo número:");
+
+const soma = Number(number1) + Number(number2);
+const subtracao = number1 - number2;
+const multiplicacao = number1 * number2;
+const divisao = number1 / number2;
+
+console.log(`Soma: ${soma}`);
+console.log(`Subtração: ${subtracao}`);
+console.log(`Multiplicação: ${multiplicacao}`);
+console.log(`Divisão: ${divisao}`);
