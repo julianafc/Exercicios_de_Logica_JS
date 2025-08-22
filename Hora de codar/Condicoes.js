9719c09ba177781cd3ab3c10952ec27c658ca432
Juliana <julianafigueirac@gmail.com>
2025-08-21 23:38:59 -0300

    feat: Condições - Hora de Codar

--- Hora de codar/Condicoes.js
+++ Hora de codar/Condicoes.js
@@ -1,1 +1,11 @@
 // Condições: Escreva um programa que pergunte ao usuário sua idade e imprima se ele é maior ou menor de idade.
+
+const prompt = require('prompt-sync')();
+
+let idade = prompt("Qual é a sua idade?");
+
+if (idade < 18) {
+    console.log("Você é menor de idade.");
+} else {
+    console.log("Você é maior de idade.");
+}
