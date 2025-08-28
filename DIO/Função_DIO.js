56f991f882813bc72f5f4ae00425b1f147e13c12
Juliana <julianafigueirac@gmail.com>
2025-08-27 21:21:31 -0300

    feat: Função- DIO

---
+++ DIO/Função_DIO.js
@@ -0,0 +1,16 @@
+function escrevaMeuNome(nome) {
+    return "Meu nome é " + nome;
+}
+
+console.log(escrevaMeuNome('Juliana'));
+
+
+function verificarIdade(idade) {
+    if (idade >= 18) {
+        console.log("Maior de idade");
+    } else {
+        console.log("Menor de idade");
+    }
+}
+
+verificarIdade(21);
